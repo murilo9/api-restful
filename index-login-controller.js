eb835050a40321254513f7ad3a90faa987f1c092
Murilo Henrique <murilomatias@alunos.utfpr.edu.br>
2018-12-11 19:53:44 -0200

    Recurso de login implementado

--- index-login-controller.js
+++ index-login-controller.js
@@ -1,8 +1,18 @@
 var indexLoginController = new Vue({
     el: "#vueLogin",
-    data: {},
-    methods: {},
-    created: function(){
-        
-    }
+    data: { login: '', senha: ''},      //Dados sob os quais esta instância Vue opera
+    methods: {
+        fazerLogin: function(){
+            var req = {login: this.login, senha: this.senha};
+            $.ajax({
+                url: 'http://localhost:8888/login',
+                data: req,
+                method: 'post',
+                statusCode: {
+                    200: function(){ alert('login ok')},
+                    401: function(){ alert('Dados incorretos.')}
+                }
+            });
+        }
+    },
 });
--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.listen(8888, function () {
         port: '3306',
         user: 'root',
         password: '',
-        database: 'orcommerce'
+        database: 'restful'
     });
 });
 
@@ -52,3 +52,71 @@ app.route('/recurso/test')      //Rota de testes
     })
     //TODO router de put
     //TODO delete router de delete
+
+    /* Router de login
+    Recebe um objeto com parâmetros usuario e senha,
+    retorna um sessionId que deve ser armazenado em um cookie no cliente
+*/
+app.route('/login')
+.post(function(req, res){
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    //Coleta os dados da request:
+    var login = req.body.login;
+    var senha = req.body.senha;
+    console.log(login);
+    console.log(senha);
+    //Define o script SQL:
+    var sql = "SELECT stSenha FROM tbUsuarios WHERE stEmail='"+ login + "'";
+    //Faz a consulta:
+    pool.query(sql, function(err, result, fields){
+        if(err){    //Em caso de erro na execução da consulta
+            console.log(err);
+            res.status(500);    //Status: 500 internal server error
+            res.end();
+        }else{
+            //Faz a leitura do resultado:
+            console.log(result[0]);
+            console.log('result lenght:'+result.lenght);
+            if(result[0].stSenha == senha){     //Caso a senha esteja correta
+                var id = Math.floor(Math.random()*99999999);     //Gera o session id
+                var expireDate = new Date();
+                expireDate.setMinutes = expireDate.getDate + 20;    //Adiciona 20 min ao session expire time
+                //Armazena esta session id com o expire date no array de sessions do server:
+                session.push({id, expireDate});
+                res.send({sessionId: id});      //Envia a response com a session id
+                console.log('login efetuado com sucesso. Id: '+ id);
+            }else{      //Caso a senha esteja incorreta
+                console.log('Senha incorreta');
+                res.status(401);    //Status: 401 unauthorized
+                res.end();
+            }
+        }
+
+    });
+});
+
+/* Router de logout 
+Recebe um objeto com parâmetro sessionId,
+retorna status 400 em caso de logout bem-sucedido
+*/
+app.route('/logout')
+.post(function(req, res){
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    //Coleta os dados da request:
+    var sessionId = req.body.sessionId;
+    //Verifica se a session id existe:
+    var sessionDestroyed = 0;
+    var i = 0;
+    while(i < session.length || !sessionDestroyed){
+        if(sessionId == obj.id && sessionDestroyed == 0){   //Se a session fornecida for igual a encontrada
+            session.splice(i, 1);       //Deleta esta session do array de sessions
+            sessionDestroyed = 1;
+        }
+    }
+    if(sessionDestroyed == 1){      //Caso a session tenha sido destruída
+        res.send('');           //Envia response vazia com status OK
+        console.log('session destruída, logout feito com sucesso');
+    }else{                  //Caso a session não tenha sido encontrada
+        res.status(404);        //Status: 404 not found
+    }
+});
