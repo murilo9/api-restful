1a5c7b865908f96dd526d12baa8e7e15d098bbbc
Murilo Henrique <murilomatias@alunos.utfpr.edu.br>
2018-12-16 20:19:53 -0200

    Sistema de cadastro implementado

--- controller/cadastro-controller.js
+++ controller/cadastro-controller.js
@@ -1,8 +1,41 @@
 var cadastroController = new Vue({
     el: "#vueCadastro",
-    data: {},
-    methods: {},
-    created: function(){
-        
+    data: { nome: '', email: '', senha: '', resenha: ''},
+    methods: {
+        validarForm: function(){
+            //Faz a validação do form
+            if(this.nome.lenght < 3){
+                alert('O nome deve ter pelo menos 3 caracteres');
+                return 0;
+            }
+            if(this.email.lenght < 7){
+                alert('Insira um email válido');
+                return 0;
+            }
+            if(this.senha != this.resenha){
+                alert('As senhas não coincidem');
+                return 0;
+            }
+            if(this.senha.lenght < 6 || this.senha.lenght > 19){
+                alert('A senha deve conter entre 6 e 20 caracteres');
+                return 0;
+            }
+            //Caso ao form tenha passado na validação, envia ao servidor:
+            var req = {nome: this.nome, email: this.email, senha: this.senha};      //Dados a serem enviados na request
+            $.ajax({
+                url: 'http://localhost:8888/cadastro',
+                data: req,
+                method: 'post',
+                statusCode:{
+                    400: function(){
+                        alert('Este email ja está cadastrado');
+                    }
+                },
+                success: function(){
+                    alert('Usuário cadastrado com sucesso');
+                    location.href="index.html";
+                }
+            });
+        }
     }
 });
--- server/server.js
+++ server/server.js
@@ -120,7 +120,7 @@ app.route('/login')
 
 /* Router de logout 
 Recebe um objeto com parâmetro sessionId,
-retorna status 400 em caso de logout bem-sucedido
+tenta eliminar a session se ela existir. Retorna sempre status 200.
 */
 app.route('/logout')
 .get(function(req, res){
@@ -145,4 +145,47 @@ app.route('/logout')
         console.log('session id não encontrada para logout: '+sessionId);
         res.end(); 
     }
+});
+
+/* Router de cadastro 
+    Recebe um objeto com parâmetros nome, email e senha,
+    retorna status 200 se o cadsatro for bem-sucedido, e 400 (bad request)
+    com uma mensagem de erro caso os dados não sejam aceitos no INSERT do DB.
+*/
+app.route('/cadastro')
+.post(function(req, res){
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    //Coleta os dados da request:
+    var email = req.body.email;
+    var nome = req.body.nome;
+    var senha = req.body.senha;
+    //Verifica se este email já está cadastrado:
+    var sql = 'SELECT stEmail FROM tbUsuarios';
+    pool.query(sql, function(err, result, fields){
+        if(err){        //Se der erro no query
+            res.status(500);        //Lança status 500 internal error
+            res.end();
+            return 0;
+        }
+        var i = 0;
+        while(i < result.length){       //Percorre o array de resultados
+            if(result[i].stEmail == email){     //Caso o email já exista
+                res.status(400);        //Retorna status 400 e mensagem de erro 
+                res.send({msg: 'Este email já está cadastrado'});
+                return 0;
+            }
+            i++;
+        }
+    });
+    //Caso o email seja válido, realiza o cadastro no BD:
+    var sql = "INSERT INTO tbUsuarios VALUES ('"+email+"','"+senha+"','"+nome+"')";
+    pool.query(sql,function(err, result, fields){
+        if(err){        //Se der erro no query
+            res.status(500);        //Lança status 500 internal error
+            res.end();
+            return 0;
+        }
+        res.status(200);        //Se a inserção foi bem-sucedida, lança status 200
+        res.end();
+    });
 });
